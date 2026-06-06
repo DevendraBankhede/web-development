02cceadea873f4a436422fc56a8a03b349206fdd
HodManagmentSystem <harshubankhede@gmail.com>
2026-06-06 20:43:12 +0530

    Refactor forms in Contect, Login, and Register components for improved structure and functionality

--- Frontend/myFirstReactApp/src/Pages/Contect.jsx
+++ Frontend/myFirstReactApp/src/Pages/Contect.jsx
@@ -1,10 +1,72 @@
-function Contect() {
+import { useState } from "react";
+
+function Login() {
+  const [FullName, setFullName] = useState("");
+  const [email, setemail] = useState("");
+  const [MobileNumber, setMobileNumber] = useState("");
+  const [conformMobileNumber, setconformMobileNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("FullName :", FullName);
+    console.log("email :", email);
+    console.log("MobileNumber :", MobileNumber);
+    console.log("conformMobileNumber :", conformMobileNumber);
+  };
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">Contect</span>
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Contact Us</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="FullName">FullName</label>
+            <input
+              type="text"
+              name="FullName"
+              className="form-control"
+              value={FullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="email">email</label>
+            <input
+              type="text"
+              name="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="MobileNumber">MobileNumber</label>
+            <input
+              type="text"
+              name="MobileNUmber"
+              className="form-control"
+              value={MobileNumber}
+              onChange={(e) => setMobileNumber(e.target.value)}
+            />
+            <br />
+            <label htmlFor="conformMobileNumber">conformMobileNumber</label>
+            <input
+              type="text"
+              name="conformMobileNumber"
+              className="form-control"
+              value={conformMobileNumber}
+              onChange={(e) => setconformMobileNumber(e.target.value)}
+            />
+            <br />
+            <div className="float-end">
+              <button type="submit" className="btn btn-primary">
+                Send a Message
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
 }
-export default Contect;
+
+export default Login;
--- Frontend/myFirstReactApp/src/Pages/Register.jsx
+++ Frontend/myFirstReactApp/src/Pages/Register.jsx
@@ -1,10 +1,73 @@
-function Register() {
+import { useState } from "react";
+
+function Login() {
+  const [FullName, setFullName] = useState("");
+  const [password, setPassword] = useState("");
+  const [MobileNumber, setMobileNumber] = useState("");
+  const [conformPassword, setconformPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("FullName :", FullName);
+    console.log("password :", password);
+    console.log("MobileNumber :", MobileNumber);
+    console.log("conformPassword :", conformPassword);
+  };
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">Register</span>
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Create Account!!!</h1>
+          <p className="text-center">Join us as a Customer, Restaurant, or Rider</p>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="FullName">FullName</label>
+            <input
+              type="text"
+              name="FullName"
+              className="form-control"
+              value={FullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <label htmlFor="conformPassword">conformPassword</label>
+            <input
+              type="text"
+              name="conformPassword"
+              className="form-control"
+              value={conformPassword}
+              onChange={(e) => setconformPassword(e.target.value)}
+            />
+            <br />
+            <label htmlFor="MobileNumber">MobileNumber</label>
+            <input
+              type="text"
+              name="MobileNUmber"
+              className="form-control"
+              value={MobileNumber}
+              onChange={(e) => setMobileNumber(e.target.value)}
+            />
+            <br />
+            <div className="float-end">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
 }
-export default Register;
+
+export default Login;
